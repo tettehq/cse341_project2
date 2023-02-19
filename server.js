ded81bd69723939adfabd966dcdb6b9a1d88e75a
tettehq <ronyquash@gmail.com>
2023-02-19 01:46:14 +0000

    cleared code on Oauth to start afresh

--- server.js
+++ server.js
@@ -2,32 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./models/connect');
 const cors = require('cors');
-const { auth, requiresAuth } = require('express-openid-connect');
-require('dotenv').config();
+
 
 const port = process.env.PORT || 8080;
 const app = express();
-
-const config = {
-  authRequired: false,
-  auth0Logout: true,
-  baseURL: process.env.BASE_URL,
-  clientID: process.env.CLIENT_ID,
-  issuerBaseURL: process.env.ISSUER_BASE_URL,
-  secret: process.env.SECRET
-};
-
-// // auth router attaches /login, /logout, and /callback routes to the baseURL
-app.use(auth(config));
-
-// req.isAuthenticated is provided from the auth router
-app.get('/', (req, res) => {
-  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out')
-});
-
-app.get('/profile', requiresAuth(), (req, res) => {
-  res.send(JSON.stringify(req.oidc.user));
-});
 
 app
   .use(cors())
